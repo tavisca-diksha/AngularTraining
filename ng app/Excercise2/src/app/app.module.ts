afc2cad264ae91b7a6900f4ebbacae5c6c8e34c0
Diksha <diksha@tavisca.com>
2020-05-15 11:57:09 +0530

    code changes

--- ng app/Excercise2/src/app/app.module.ts
+++ ng app/Excercise2/src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieComponent } from './Components/Movies/app.movie.component';
 import { MovieReactiveFormComponent } from './Components/MoviesReactive/app.moviereactiveform.component';
 import { TableDirectiveComponent } from './Directives/table.component.directive';
+import { DatagridElementComponent } from './LitElementComponents/app.datagrid.element.component';
 
 @NgModule({
   declarations: [
-    AppComponent, MovieComponent, MovieReactiveFormComponent, TableDirectiveComponent
+    AppComponent, MovieComponent, MovieReactiveFormComponent, TableDirectiveComponent, DatagridElementComponent
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [MovieReactiveFormComponent]
+  bootstrap: [DatagridElementComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
