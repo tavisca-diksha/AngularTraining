6f4562e3f07b055d2562f7694dade41456a79dbe
Diksha <diksha@tavisca.com>
2020-05-18 10:59:03 +0530

    added day 4 assignment

--- ng app/Excercise2/src/app/app.module.ts
+++ ng app/Excercise2/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +15,7 @@ import { TableDirectiveComponent } from './Directives/table.component.directive'
     BrowserModule, FormsModule, ReactiveFormsModule,
     AppRoutingModule
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [MovieReactiveFormComponent]
 })
